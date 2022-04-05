33c50ab62cbfd507fffb24c29a55a46aa8a0e5b0
developeralamin <ribinaofficial018@gmail.com>
2022-04-05 18:09:07 +0600

    add meta tag

--- src/router/index.js
+++ src/router/index.js
@@ -33,32 +33,35 @@ import ShowAbout from '@/views/about/ShowAbout.vue'
 import Login from '@/views/auth/Login.vue'
 import Register from '@/views/auth/Register.vue'
 
+var appname = " - Single Page Application";
 
 
 const routes = [
-    
+
     {
         path: '/',
         name: 'Home',
         component: Home,
+         meta: { title: "Home" + appname },
     },
 
-     // category section
+    // category section
     // category section
 
     {
         path: '/category',
         name: 'Category',
         component: Category,
+         meta: { title: "Category" + appname },
     },
 
-     {
+    {
         path: '/category/create',
         name: 'create-category',
         component: CreateCategory,
     },
 
-      {
+    {
         path: '/category/edit/:id',
         name: 'edit-category',
         component: EditCategory,
@@ -69,16 +72,16 @@ const routes = [
     {
         path: '/lesson',
         name: 'lesson',
-        component:Lesson
-        
+        component: Lesson,
+         meta: { title: "lesson" + appname },
     },
 
-      {
+    {
         path: '/lesson/create',
         name: 'create-lesson',
-        component:CreateLesson
-        
-     },
+        component: CreateLesson
+
+    },
 
     {
         path: '/lesson/edit/:id',
@@ -86,28 +89,29 @@ const routes = [
         component: Editlesson,
     },
 
-     {
+    {
         path: '/lesson/:id',
         name: 'show-lesson',
         component: Showlesson,
     },
-      
+
     // course section
     // course section   
     {
         path: '/course',
         name: 'course',
-        component:Course
-    },
-   
-       {
-        path: '/course/reate',
-        name: 'create-course',
-        component:CreateCourse
-        
+        component: Course,
+          meta: { title: "course" + appname },
     },
 
-   {
+    {
+        path: '/course/reate',
+        name: 'create-course',
+        component: CreateCourse
+
+    },
+
+    {
         path: '/course/edit/:id',
         name: 'edit-course',
         component: EditCourse,
@@ -115,94 +119,106 @@ const routes = [
 
     // bennar section
     // bennar section   
-     {
+    {
         path: '/bennar',
         name: 'bennar',
-        component:Bennar
+        component: Bennar,
+          meta: { title: "bennar" + appname },
     },
-    
-      {
+
+    {
         path: '/bennar/create',
         name: 'create-bennar',
-        component:CreateBennar
-        
-     },
-     {
+        component: CreateBennar
+
+    },
+    {
         path: '/bennar/edit/:id',
         name: 'edit-bennar',
-        component:EditBennar
-        
+        component: EditBennar
+
     },
-  //about section
-      {
+    //about section
+    //about section
+    {
         path: '/about',
         name: 'about',
-        component:About
+        component: About,
+         meta: { title: "about" + appname },
     },
-     
- {
+
+    {
         path: '/about/create',
         name: 'create-about',
-        component:CreateAbout
-        
-     },
-     {
+        component: CreateAbout
+
+    },
+    {
         path: '/about/edit/:id',
         name: 'edit-about',
-        component:EditAbout
-        
+        component: EditAbout
+
     },
-     
-          {
+
+    {
         path: '/about/show/:id',
         name: 'show-about',
-        component:ShowAbout
-        
+        component: ShowAbout
+
     },
     //course details show 
-     
-     {
-           path: '/course/:id',
-            component: CourseDetails,
-            name: 'course-details',
-     },
-   
-      {
-            path: '/login',
-            component: Login,
-            name: 'login',
-      },
+
+    {
+        path: '/course/:id',
+        component: CourseDetails,
+        name: 'course-details',
+         meta: { title: "CourseDetails" + appname },
+    },
+
+    {
+        path: '/login',
+        component: Login,
+        name: 'login',
+         meta: { title: "login" + appname },
+    },
 
     {
         path: '/register',
         name: 'register',
-        component:Register
+        component: Register,
+          meta: { title: "register" + appname },
     },
 
 
-   // Not Found Page
+    // Not Found Page
     {
-         path: '/:catchAll(.*)*',
+        path: '/:catchAll(.*)*',
         name: "NotFound",
-        component:PageNotFound
-    },  
-    
+        component: PageNotFound,
+          meta: { title: "Home" + appname },
+    },
+
 ];
 
 const router = createRouter({
-   history: createWebHistory(),
-   routes,
+    history: createWebHistory(),
+    routes,
 
 
- scrollBehavior (to, from, savedPosition) {
+    scrollBehavior(to, from, savedPosition) {
         return savedPosition || new Promise((resolve) => {
             setTimeout(() => {
-                  resolve({top:0 ,  behavior: 'smooth',})
+                resolve({ top: 0, behavior: 'smooth', })
             }, 3000);
-        } )
-          
+        })
+
     }
 
+});
+
+router.beforeEach((to, from, next) => {
+  document.title = to.meta.title;
+  next();
 });
 
 export default router;
